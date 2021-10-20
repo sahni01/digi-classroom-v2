404fa80a780b4f3f5d6a477d48afb0e0ca8a362c
Nitesh <sahninitesh01@gmail.com>
2021-10-20 18:24:28 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.Server(app);
+const io = require('socket.io')(server);
+const {v4:uuidV4} = require('uuid')
+const {ExpressPeerServer} = require('peer')
+
+const peerServer = ExpressPeerServer(server,{
+    debug:true,
+})
+app.use('/peerjs',peerServer)
+const PORT = process.env.PORT||3000
+
+app.use(express.static('public'))
+app.set('view engine','ejs')
+
+
+
+
+
+
+
+
+app.get('/',async(req,res)=>{
+    res.redirect(`${uuidV4()}`)
+})
+
+app.get('/:roomId',(req,res)=>{
+    res.render('class',{roomId:req.params.roomId})
+})
+
+
+io.on('connection',(socket)=>{
+    console.log('user connected')
+    socket.on('join-room',(roomId,userId)=>{
+        console.log(roomId);
+        socket.join(roomId)
+        console.log(userId)
+        socket.to(roomId).emit('user-connected',userId)
+        console.log(roomId,userId)
+    })
+})
+
+
+
+server.listen(PORT,()=>{
+    console.log(`server is listenning at http://localhost:${PORT}`)
+})
---
+++ public/script.js
@@ -0,0 +1,73 @@
+const bt = document.getElementById('bt')
+bt.addEventListener('click',connect)
+
+
+function connect(){
+
+    const socket = io('/')
+    
+    
+    var mypeer = new Peer(undefined,{
+        path:'/peerjs',
+        host:'/',
+        port:3000
+        
+    })
+    mypeer.on('open',id=>{
+        socket.emit('join-room',Room_Id,id)
+        
+    })
+    
+    
+    const video = document.createElement('video');
+    video.muted=true
+    // video.autoplay=false
+    const videoBox = document.getElementsByClassName('video-box')[0]
+    
+    function load(){
+    
+        navigator.mediaDevices.getUserMedia({
+            video:true,
+            audio:true
+        }).then(stream=>{
+            
+            addvideostream(video,stream)
+        
+            socket.on('user-connected',(userId)=>{
+                console.log('user-connected: '+userId)
+                connecttonewuser(userId,stream)
+            })
+        
+            mypeer.on('call',call=>{
+                call.answer(stream)
+                const video =  document.createElement('video')
+                call.on('stream',uservideosstream=>{
+                    addvideostream(video,uservideosstream)
+                })
+            })
+        })
+    }
+    load();
+    function addvideostream(video,stream){
+        video.srcObject = stream
+        video.addEventListener('loadedmetadata',()=>{
+            video.play()
+        })
+        videoBox.append(video);
+    
+    }
+    
+    function connecttonewuser(userId,stream){
+        const call = mypeer.call(userId,stream)
+        const video = document.createElement('video')
+        call.on('stream',uservideosstream=>{
+            addvideostream(video,uservideosstream)
+        })
+        call.on('close',()=>{
+            video.remove()
+        })
+    }
+
+    
+}
+connect();
