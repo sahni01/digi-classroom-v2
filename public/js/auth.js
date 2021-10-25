ae774adbf634e022ec3a0fd7248880c07d7d1999
Nitesh <sahninitesh01@gmail.com>
2021-10-25 17:05:33 +0530

    make it better

--- public/js/auth.js
+++ public/js/auth.js
@@ -3,7 +3,7 @@ const join = document.getElementById('join-class-button');
 
 join.addEventListener('click',()=>{
     const roomid = document.getElementById('roomid').value;
-    const password = document.getElementById('password').value;
+    const password = document.getElementById('join-password').value;
 
     window.location.href=`/join/${roomid}?password=${password}`;
 })
